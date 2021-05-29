e5616aca590046f646c6582cddca0171f47d891b
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2021-05-29 13:01:34 +0200

    Fix oshd_cmd.h function prototypes
    The return values were invalid

--- include/oshd_cmd.h
+++ include/oshd_cmd.h
@@ -3,9 +3,9 @@
 
 #include <stdbool.h>
 
-bool oshd_cmd_set(const char *name, const char *cmdline);
+void oshd_cmd_set(const char *name, const char *cmdline);
 void oshd_cmd_unset(const char *name);
 void oshd_cmd_unset_all(void);
-int oshd_cmd_execute(const char *name);
+bool oshd_cmd_execute(const char *name);
 
 #endif
