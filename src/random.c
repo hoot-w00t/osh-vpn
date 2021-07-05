9e959743370b941d3be3c53d2df5e34388b25d6a
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2021-07-05 16:20:48 +0200

    Log more information if read_random_bytes fails

--- src/random.c
+++ src/random.c
@@ -5,20 +5,24 @@
 #include <string.h>
 #include <errno.h>
 
+#define random_filepath "/dev/random"
+
 // Read buf_size random bytes starting at buf
 bool read_random_bytes(uint8_t *buf, size_t buf_size)
 {
-    int fd = open("/dev/random", O_RDONLY);
+    int fd = open(random_filepath, O_RDONLY);
     size_t total = 0;
     ssize_t rbytes;
 
     if (fd < 0) {
-        logger(LOG_ERR, "read_random_bytes: open: %s", strerror(errno));
+        logger(LOG_ERR, "read_random_bytes: open: %s: %s",
+            random_filepath, strerror(errno));
         return false;
     }
     while (total < buf_size) {
         if ((rbytes = read(fd, buf + total, buf_size - total)) <= 0) {
-            logger(LOG_ERR, "read_random_bytes: read: %s", strerror(errno));
+            logger(LOG_ERR, "read_random_bytes: read: %s: %s (%zi, %zu/%zu bytes)",
+                random_filepath, strerror(errno), rbytes, total, buf_size);
             break;
         }
         total += rbytes;
