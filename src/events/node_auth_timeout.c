feb0e6eed3cd63192c13d59185653c2af61eeac6
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2021-10-26 15:20:29 +0200

    Harden node_auth_timeout event
    Make sure to never overwrite the node_auth_timeout event pointer in the
    linked node_t

--- src/events/node_auth_timeout.c
+++ src/events/node_auth_timeout.c
@@ -8,13 +8,13 @@ static time_t node_auth_timeout_event_handler(void *data)
 {
     node_t *node = (node_t *) data;
 
+    node->auth_timeout_event = NULL;
     if (!node->authenticated) {
         if (node->connected) {
             logger(LOG_WARN, "%s: Authentication timed out", node->addrw);
         } else {
             logger(LOG_WARN, "%s: Timed out", node->addrw);
         }
-        node->auth_timeout_event = NULL;
         aio_event_del(node->aio_event);
     }
     return EVENT_IS_DONE;
@@ -27,12 +27,14 @@ static void node_auth_timeout_event_freedata(void *data)
 
 void event_queue_node_auth_timeout(node_t *node, time_t timeout_delay)
 {
-    event_t *event = event_create(
-        "node_auth_timeout",
-        node_auth_timeout_event_handler,
-        node_auth_timeout_event_freedata,
-        node);
-
-    node->auth_timeout_event = event;
-    event_queue_in(event, timeout_delay);
+    if (node->auth_timeout_event) {
+        logger(LOG_WARN, "%s: node_auth_timeout event already exists", node->addrw);
+    } else {
+        node->auth_timeout_event = event_create(
+            "node_auth_timeout",
+            node_auth_timeout_event_handler,
+            node_auth_timeout_event_freedata,
+            node);
+    }
+    event_queue_in(node->auth_timeout_event, timeout_delay);
 }
