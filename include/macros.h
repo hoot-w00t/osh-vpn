1367b5871d8847642e179b81540c8cbe01996073
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2022-05-11 14:36:15 +0200

    Add PLATFORM_IS_* macros

--- include/macros.h
+++ include/macros.h
@@ -5,4 +5,12 @@
 
 #define IO_WOULDBLOCK(err) ((err) == EAGAIN || (err) == EWOULDBLOCK)
 
+#define PLATFORM_IS_LINUX (__linux__)
+#define PLATFORM_IS_WINDOWS (_WIN32 || __CYGWIN__)
+
+// Only one of these macros should be true
+#if PLATFORM_IS_LINUX && PLATFORM_IS_WINDOWS
+#error "Platform detection is broken"
+#endif
+
 #endif
