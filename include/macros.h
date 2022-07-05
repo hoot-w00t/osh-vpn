fa4e6e15c8d6fc4698e82d65a8c2363726816f40
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2022-07-05 22:36:18 +0200

    Refactor platform detection
    Manually define PLATFORM_IS_* to 1
    Use PLATFORM_COUNT macro to check for correct detection

--- include/macros.h
+++ include/macros.h
@@ -5,16 +5,26 @@
 
 #define IO_WOULDBLOCK(err) ((err) == EAGAIN || (err) == EWOULDBLOCK)
 
-#define PLATFORM_IS_LINUX (__linux__)
-#define PLATFORM_IS_WINDOWS (_WIN32 || __CYGWIN__ || __MSYS__)
-
-// Only one of these macros should be true
-#if PLATFORM_IS_LINUX && PLATFORM_IS_WINDOWS
-#error "Platform detection is broken"
+// Platform detection
+#if defined(__linux__)
+    #define PLATFORM_IS_LINUX 1
 #endif
 
-// But at least one should match
-#if !PLATFORM_IS_LINUX && !PLATFORM_IS_WINDOWS
+#if defined(_WIN32) || defined(__CYGWIN__) || defined(__MSYS__)
+    #define PLATFORM_IS_WINDOWS 1
+#endif
+
+// This macro should always equal to 1
+#define PLATFORM_COUNT (PLATFORM_IS_LINUX + PLATFORM_IS_WINDOWS)
+
+// We can only have one platform defined
+#if PLATFORM_COUNT > 1
+#error "Multiple platforms were recognized"
+#endif
+
+// We should always have a platform defined (even if having none does not
+// prevent the code from compiling and working)
+#if PLATFORM_COUNT <= 0
 #warning "No platform was recognized"
 #endif
 
