ad148bc306a800a263b76cbf98946c6732343e4e
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2023-01-06 15:02:18 +0100

    Add CMake module to configure compiler flags

---
+++ cmake/compiler_flags.cmake
@@ -0,0 +1,37 @@
+cmake_minimum_required(VERSION 3.3.0)
+
+include(CheckCCompilerFlag)
+
+# Add ${flag} to ${destvar}
+function(add_compiler_flag_nocheck destvar flag)
+    set(${destvar} "${${destvar}} ${flag}" PARENT_SCOPE)
+endfunction()
+
+# Add ${ARGN} flags to ${destvar}
+function(add_compiler_flags_nocheck destvar)
+    foreach(flag ${ARGN})
+        add_compiler_flag_nocheck(${destvar} "${flag}")
+    endforeach()
+    set(${destvar} "${${destvar}}" PARENT_SCOPE)
+endfunction()
+
+# Add ${flag} to ${destvar} if the flag is supported by the compiler
+function(add_compiler_flag destvar flag)
+    string(REPLACE "-" "_" checkvar "cflag${flag}")
+    string(REPLACE "=" "_" checkvar "${checkvar}")
+    string(TOLOWER "${checkvar}" checkvar)
+
+    check_c_compiler_flag("${flag}" ${checkvar})
+    if (${checkvar})
+        add_compiler_flag_nocheck(${destvar} "${flag}")
+    endif()
+    set(${destvar} "${${destvar}}" PARENT_SCOPE)
+endfunction()
+
+# Add ${ARGN} flags to ${destvar} if the flags are supported by the compiler
+function(add_compiler_flags destvar)
+    foreach(flag ${ARGN})
+        add_compiler_flag(${destvar} "${flag}")
+    endforeach()
+    set(${destvar} "${${destvar}}" PARENT_SCOPE)
+endfunction()
