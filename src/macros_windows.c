e83da5f6470d912ff1d7bf7a17dfd1c3b9f33a8c
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2023-07-11 18:33:52 +0200

    Fix win_strerror() memory errors and newline removal

--- src/macros_windows.c
+++ src/macros_windows.c
@@ -1,40 +1,34 @@
 #include "macros_windows.h"
 #include <stdio.h>
 
+#define is_newline(x) ((x) == '\n' || (x) == '\r')
+#define errfmt_maxlen 256
+
 // Equivalent to strerror()
-// The returned pointer is a static char array
+// The returned pointer is a static char array, this function is not thread-safe
 const char *win_strerror(DWORD errcode)
 {
-    static char errfmt[256];
-    static char errstr[sizeof(errfmt) + 32];
+    static char errstr[errfmt_maxlen + 32];
+    char errfmt[errfmt_maxlen];
     DWORD fmterr;
 
     fmterr = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, errcode, 0,
         errfmt, sizeof(errfmt), NULL);
 
     if (fmterr) {
-        // Remove the newline at the end of the error string
-        // TODO: There could be a better way of doing this, this is very ugly
-        size_t errstr_len = strlen(errstr);
+        // Remove all newlines at the end of the error string
+        size_t errfmt_len = strlen(errfmt);
 
-        if (   errstr_len > 0
-            && (errstr[errstr_len - 1] == '\n' || errstr[errstr_len - 1] == '\r'))
-        {
-            errstr[errstr_len - 1] = '\0';
-            errstr_len -= 1;
-            if (   errstr_len > 0
-                && (errstr[errstr_len - 1] == '\n' || errstr[errstr_len - 1] == '\r'))
-            {
-                errstr[errstr_len - 1] = '\0';
-                errstr_len -= 1;
-            }
+        while (errfmt_len > 0 && is_newline(errfmt[errfmt_len - 1])) {
+            errfmt_len -= 1;
+            errfmt[errfmt_len] = '\0';
         }
     }
 
     if (fmterr) {
         snprintf(errstr, sizeof(errstr), "%s (code %lu)", errfmt, errcode);
     } else {
-        snprintf(errstr, sizeof(errstr), "Error code %lu (FormatMessage failed with %lu)",
+        snprintf(errstr, sizeof(errstr), "Error code %lu (FormatMessageA failed with %lu)",
             errcode, GetLastError());
     }
     return errstr;
