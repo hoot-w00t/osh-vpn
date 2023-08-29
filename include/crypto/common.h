790d10ab93099ea55f2c495f157910490efcbfc3
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2023-08-29 11:54:03 +0200

    Add osh_openssl_log_error() macro in crypto/common.h

--- include/crypto/common.h
+++ include/crypto/common.h
@@ -2,10 +2,13 @@
 #define _OSH_CRYPTO_COMMON_H
 
 #include "memzero.h"
+#include "logger.h"
 #include <openssl/err.h>
 
-#ifndef osh_openssl_strerror
-#define osh_openssl_strerror ERR_error_string(ERR_get_error(), NULL)
-#endif
+#define osh_openssl_strerror \
+    ERR_error_string(ERR_get_error(), NULL)
+
+#define osh_openssl_log_error(ossl_funcname) \
+    logger(LOG_ERR, "%s: %s: %s", __func__, ossl_funcname, osh_openssl_strerror)
 
 #endif
