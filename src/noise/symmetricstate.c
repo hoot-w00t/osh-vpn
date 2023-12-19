463990bc6770a722ac1550cb695c2ee44e1aa696
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2023-12-19 01:46:09 +0100

    Cherry-pick Noise symmetricstate from Noise dev branch

---
+++ src/noise/symmetricstate.c
@@ -0,0 +1,277 @@
+#include "noise/symmetricstate.h"
+#include "crypto/hash.h"
+#include "macros_assert.h"
+#include "xalloc.h"
+#include "memzero.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct noise_symmetricstate {
+    cipher_type_t cipher_type;
+    noise_cipherstate_t *cipher;
+
+    size_t keylen;
+
+    hash_type_t hash_type;
+    size_t hash_len;
+    hash_ctx_t *hash_ctx;
+
+    uint8_t *chaining_key;
+    uint8_t *hash;
+};
+
+__attribute__((warn_unused_result))
+static bool noise_hkdf(noise_symmetricstate_t *ctx,
+    const void *ikm, size_t ikm_len,
+    size_t num_outputs,
+    void *output1, void *output2, void *output3)
+{
+    const char label = 0;
+    bool success = false;
+    size_t outlen = 0;
+    uint8_t *out = NULL;
+
+    assert(num_outputs == 2 || num_outputs == 3);
+    outlen = num_outputs * ctx->hash_len;
+    out = xzalloc(outlen);
+
+    success = hash_hkdf(ctx->hash_type, ikm, ikm_len, ctx->chaining_key, ctx->hash_len, &label, 0, out, outlen);
+    if (success) {
+        memcpy(output1, out + (0 * ctx->hash_len), ctx->hash_len);
+        memcpy(output2, out + (1 * ctx->hash_len), ctx->hash_len);
+        if (num_outputs == 3)
+            memcpy(output3, out + (2 * ctx->hash_len), ctx->hash_len);
+    }
+
+    memzero(out, outlen);
+    free(out);
+    return success;
+}
+
+noise_symmetricstate_t *noise_symmetricstate_create(const char *protocol_name,
+    cipher_type_t cipher_type, hash_type_t hash_type)
+{
+    noise_symmetricstate_t *ctx = xzalloc(sizeof(*ctx));
+    const size_t name_len = strlen(protocol_name);
+
+    ctx->cipher_type = cipher_type;
+    ctx->cipher = noise_cipherstate_create(ctx->cipher_type, false);
+    if (ctx->cipher == NULL)
+        goto fail;
+
+    ctx->keylen = cipher_get_key_size_from_type(ctx->cipher_type);
+    ctx->hash_type = hash_type;
+    ctx->hash_len = hash_type_length(ctx->hash_type);
+    ctx->hash_ctx = hash_ctx_create(ctx->hash_type);
+    if (ctx->hash_ctx == NULL)
+        goto fail;
+
+    assert(ctx->keylen > 0);
+    assert(ctx->hash_len > 0);
+    assert(ctx->hash_len >= ctx->keylen); // we get keys from hashes (truncated if KEYLEN is smaller than HASHLEN)
+                                          // so HASHLEN must not be smaller than KEYLEN
+
+    ctx->chaining_key = xzalloc(ctx->hash_len);
+    ctx->hash = xzalloc(ctx->hash_len);
+
+    if (name_len <= ctx->hash_len) {
+        memcpy(ctx->hash, protocol_name, name_len);
+    } else {
+        if (!(   hash_ctx_reset(ctx->hash_ctx)
+              && hash_ctx_update(ctx->hash_ctx, protocol_name, name_len)
+              && hash_ctx_final(ctx->hash_ctx, ctx->hash, ctx->hash_len)))
+        {
+            goto fail;
+        }
+    }
+    memcpy(ctx->chaining_key, ctx->hash, ctx->hash_len);
+
+    // We expect this to return false since we reset the key
+    if (noise_cipherstate_initialize_key(ctx->cipher, NULL, 0))
+        goto fail;
+
+    return ctx;
+
+fail:
+    noise_symmetricstate_destroy(ctx);
+    return NULL;
+}
+
+void noise_symmetricstate_destroy(noise_symmetricstate_t *ctx)
+{
+    if (ctx) {
+        if (ctx->chaining_key != NULL)
+            memzero_free(ctx->chaining_key, ctx->hash_len);
+        if (ctx->hash != NULL)
+            memzero_free(ctx->hash, ctx->hash_len);
+
+        hash_ctx_free(ctx->hash_ctx);
+        noise_cipherstate_destroy(ctx->cipher);
+        free(ctx);
+    }
+}
+
+bool noise_symmetricstate_has_key(noise_symmetricstate_t *ctx)
+{
+    return noise_cipherstate_has_key(ctx->cipher);
+}
+
+bool noise_symmetricstate_mix_key(noise_symmetricstate_t *ctx, const void *ikm, size_t ikm_len)
+{
+    bool success = false;
+    uint8_t *ck_out1 = xzalloc(ctx->hash_len);
+    uint8_t *temp_k_out2 = xzalloc(ctx->hash_len);
+
+    if (!noise_hkdf(ctx, ikm, ikm_len, 2, ck_out1, temp_k_out2, NULL))
+        goto end;
+
+    memcpy(ctx->chaining_key, ck_out1, ctx->hash_len);
+    if (!noise_cipherstate_initialize_key(ctx->cipher, temp_k_out2, ctx->keylen))
+        goto end;
+
+    success = true;
+
+end:
+    memzero_free(ck_out1, ctx->hash_len);
+    memzero_free(temp_k_out2, ctx->hash_len);
+    return success;
+}
+
+bool noise_symmetricstate_mix_hash(noise_symmetricstate_t *ctx, const void *data, size_t data_len)
+{
+    return hash_ctx_reset(ctx->hash_ctx)
+        && hash_ctx_update(ctx->hash_ctx, ctx->hash, ctx->hash_len)
+        && hash_ctx_update(ctx->hash_ctx, data, data_len)
+        && hash_ctx_final(ctx->hash_ctx, ctx->hash, ctx->hash_len);
+}
+
+// same as mix_hash but hashes data1 then data2 (avoids byte array concats)
+bool noise_symmetricstate_mix_hash_2(noise_symmetricstate_t *ctx,
+    const void *data1, size_t data1_len,
+    const void *data2, size_t data2_len)
+{
+    return hash_ctx_reset(ctx->hash_ctx)
+        && hash_ctx_update(ctx->hash_ctx, ctx->hash, ctx->hash_len)
+        && hash_ctx_update(ctx->hash_ctx, data1, data1_len)
+        && hash_ctx_update(ctx->hash_ctx, data2, data2_len)
+        && hash_ctx_final(ctx->hash_ctx, ctx->hash, ctx->hash_len);
+}
+
+bool noise_symmetricstate_mix_key_and_hash(noise_symmetricstate_t *ctx, const void *ikm, size_t ikm_len)
+{
+    bool success = false;
+    uint8_t *ck_out1 = xzalloc(ctx->hash_len);
+    uint8_t *temp_h_out2 = xzalloc(ctx->hash_len);
+    uint8_t *temp_k_out3 = xzalloc(ctx->hash_len);
+
+    if (!noise_hkdf(ctx, ikm, ikm_len, 3, ck_out1, temp_h_out2, temp_k_out3))
+        goto end;
+
+    memcpy(ctx->chaining_key, ck_out1, ctx->hash_len);
+    if (!noise_symmetricstate_mix_hash(ctx, temp_h_out2, ctx->hash_len))
+        goto end;
+    if (!noise_cipherstate_initialize_key(ctx->cipher, temp_k_out3, ctx->keylen))
+        goto end;
+
+    success = true;
+
+end:
+    memzero_free(ck_out1, ctx->hash_len);
+    memzero_free(temp_h_out2, ctx->hash_len);
+    memzero_free(temp_k_out3, ctx->hash_len);
+    return success;
+}
+
+const uint8_t *noise_symmetricstate_get_handshake_hash(const noise_symmetricstate_t *ctx)
+{
+    return ctx->hash;
+}
+
+bool noise_symmetricstate_encrypt_and_hash(noise_symmetricstate_t *ctx,
+    const void *plaintext, size_t plaintext_len,
+    void *output, size_t *output_len,
+    void *mac, size_t mac_len)
+{
+    if (!noise_cipherstate_encrypt_with_ad_postinc(ctx->cipher,
+            ctx->hash, ctx->hash_len, plaintext, plaintext_len,
+            output, output_len, mac, mac_len))
+    {
+        return false;
+    }
+
+    if (noise_cipherstate_has_key(ctx->cipher)) {
+        return noise_symmetricstate_mix_hash_2(ctx, output, *output_len, mac, mac_len);
+    } else {
+        return noise_symmetricstate_mix_hash(ctx, output, *output_len);
+    }
+    return true;
+}
+
+bool noise_symmetricstate_decrypt_and_hash(noise_symmetricstate_t *ctx,
+    const void *ciphertext, size_t ciphertext_len,
+    void *output, size_t *output_len,
+    void *mac, size_t mac_len)
+// FIXME: Make *mac const once noise_cipherstate_decrypt_with_ad_postinc() *mac becomes const
+{
+    if (!noise_cipherstate_decrypt_with_ad_postinc(ctx->cipher,
+            ctx->hash, ctx->hash_len, ciphertext, ciphertext_len,
+            output, output_len, mac, mac_len))
+    {
+        return false;
+    }
+
+    if (noise_cipherstate_has_key(ctx->cipher)) {
+        return noise_symmetricstate_mix_hash_2(ctx, ciphertext, ciphertext_len, mac, mac_len);
+    } else {
+        return noise_symmetricstate_mix_hash(ctx, ciphertext, ciphertext_len);
+    }
+
+    return true;
+}
+
+bool noise_symmetricstate_split(noise_symmetricstate_t *ctx,
+    noise_cipherstate_t **c1, noise_cipherstate_t **c2)
+{
+    uint8_t *temp_k1 = xzalloc(ctx->hash_len);
+    uint8_t *temp_k2 = xzalloc(ctx->hash_len);
+    const uint8_t empty = 0;
+    bool success = false;
+
+    assert(c1 != NULL);
+    assert(c2 != NULL);
+    assert(c1 != c2);
+
+    // Both ciphers will be accessed on error so they have to be initialized here first
+    *c1 = NULL;
+    *c2 = NULL;
+
+    if (!noise_hkdf(ctx, &empty, 0, 2, temp_k1, temp_k2, NULL))
+        goto end;
+
+    *c1 = noise_cipherstate_create(ctx->cipher_type, true);
+    if (*c1 == NULL)
+        goto end;
+    if (!noise_cipherstate_initialize_key(*c1, temp_k1, ctx->keylen))
+        goto end;
+
+    *c2 = noise_cipherstate_create(ctx->cipher_type, true);
+    if (*c2 == NULL)
+        goto end;
+    if (!noise_cipherstate_initialize_key(*c2, temp_k2, ctx->keylen))
+        goto end;
+
+    success = true;
+
+end:
+    memzero_free(temp_k1, ctx->hash_len);
+    memzero_free(temp_k2, ctx->hash_len);
+    if (!success) {
+        noise_cipherstate_destroy(*c1);
+        *c1 = NULL;
+        noise_cipherstate_destroy(*c2);
+        *c2 = NULL;
+    }
+    return success;
+}
