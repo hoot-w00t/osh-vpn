17ceb41d8a7a665ebc68a4c1c035b683af70dad7
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2023-12-28 17:43:05 +0100

    Refactor bitfield unit tests to test with 64-bit integers

--- tests/macros_bitfield_tests.c
+++ tests/macros_bitfield_tests.c
@@ -2,28 +2,34 @@
 #include <stdint.h>
 #include <criterion/criterion.h>
 
+typedef uint64_t                    test_type_t;
+#define test_type_width             64
+#define test_type_c                 UINT64_C
+#define test_type_max               UINT64_MAX
+#define test_type_f0                test_type_c(0xF0F0F0F0F0F0F0F0)
+
 Test(BIT_SET, test_bit_set_macro)
 {
-    uint32_t u   = 0;
-    uint32_t ref = 0;
+    test_type_t u   = 0;
+    test_type_t ref = 0;
 
-    for (int i = 0; i < 32; ++i) {
+    for (int i = 0; i < test_type_width; ++i) {
         BIT_SET(u, i);
-        ref |= ((unsigned) 1 << i);
+        ref |= (test_type_c(1) << i);
         cr_assert_eq(u, ref);
     }
-    cr_assert_eq(u, 0xFFFFFFFFu);
-    cr_assert_eq(ref, 0xFFFFFFFFu);
+    cr_assert_eq(u, test_type_max);
+    cr_assert_eq(ref, test_type_max);
 }
 
 Test(BIT_CLEAR, test_bit_clear_macro)
 {
-    uint32_t u   = 0xFFFFFFFFu;
-    uint32_t ref = 0xFFFFFFFFu;
+    test_type_t u   = test_type_max;
+    test_type_t ref = test_type_max;
 
-    for (int i = 0; i < 32; ++i) {
+    for (int i = 0; i < test_type_width; ++i) {
         BIT_CLEAR(u, i);
-        ref &= ~((unsigned) 1 << i);
+        ref &= ~(test_type_c(1) << i);
         cr_assert_eq(u, ref);
     }
     cr_assert_eq(u, 0);
@@ -32,16 +38,32 @@ Test(BIT_CLEAR, test_bit_clear_macro)
 
 Test(BIT_TEST, test_bit_test_macro)
 {
-    const uint32_t u = 0xF0F0F0F0;
+    const test_type_t u = test_type_f0;
 
-    cr_assert_eq(BIT_TEST(u, 30), 0x40000000u);
-    cr_assert_eq(BIT_TEST(u, 26), 0);
+    for (int bit = 0; bit < test_type_width; ++bit) {
+        test_type_t value;
+
+        if ((bit % 8) < 4)
+            value = 0;
+        else
+            value = test_type_c(1) << bit;
+
+        cr_assert_eq(BIT_TEST(u, bit), value);
+    }
 }
 
 Test(BIT_GET, test_bit_get_macro)
 {
-    const uint32_t u = 0xF0F0F0F0;
+    const test_type_t u = test_type_f0;
 
-    cr_assert_eq(BIT_GET(u, 22), 1u);
-    cr_assert_eq(BIT_GET(u, 16), 0);
+    for (int bit = 0; bit < test_type_width; ++bit) {
+        test_type_t value;
+
+        if ((bit % 8) < 4)
+            value = 0;
+        else
+            value = 1;
+
+        cr_assert_eq(BIT_GET(u, bit), value);
+    }
 }
